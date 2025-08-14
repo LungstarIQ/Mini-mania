19ca85df1a6bf2127aa0c5fa998e20fe742d340e
LungstarIQ <thandananimkhize538@gmail.com>
2025-08-14 15:35:20 +0200

    first push

---
+++ flipping-game/src/main.ts
@@ -0,0 +1,14 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
+});
