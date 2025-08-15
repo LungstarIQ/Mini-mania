9ae063bbcce68cf75b4b9862a7c3916a8360a03a
LungstarIQ <thandananimkhize538@gmail.com>
2025-08-15 17:39:14 +0200

    Created Login Page and optimized routing

---
+++ flipping-game/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCkrQVnvjNWvXMyrTg89lBBNysbebkYVz8",
+    authDomain: "mini-mania-f351c.firebaseapp.com",
+    projectId: "mini-mania-f351c",
+    storageBucket: "mini-mania-f351c.firebasestorage.app",
+    messagingSenderId: "670877795191",
+    appId: "1:670877795191:web:e61d17434e41f7b2989772",
+    measurementId: "G-2GYT7JC5Y2"
+  }
+};
--- flipping-game/src/main.ts
+++ flipping-game/src/main.ts
@@ -1,14 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    provideRouter(routes),
   ]
 });
