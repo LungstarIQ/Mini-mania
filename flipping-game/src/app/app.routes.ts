eadd35feb4edea1e8dd71cae5f23ced4a09854d4
LungstarIQ <thandananimkhize538@gmail.com>
2025-08-16 16:11:52 +0200

    pull request testing

--- flipping-game/src/app/app.routes.ts
+++ flipping-game/src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from '../guards/auth.guard';
 import { HomeComponent } from './home/home.component';
+import { CardGameComponent } from './card-game/card-game.component';
+import { RoadmapComponent } from './roadmap/roadmap.component';
 
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'cardGame', component: CardGameComponent, canActivate: [AuthGuard] },
+  { path: 'map', component: RoadmapComponent, canActivate: [AuthGuard] },
+
 ];
